fca30a4453c3a4238d63d5e0ac381c039031c726
AlisherU <alisherrahmonov@gmail.com>
2021-08-17 11:07:02 +0900

    Create routes for update, delete and search

--- src/actions/item.js
+++ src/actions/item.js
@@ -5,6 +5,9 @@ const url = "http://localhost:5000/items";
 
 const fetchItemsAPI = () => axios.get(url);
 const createItemAPI = (newItem) => axios.post(url, newItem);
+const updateItemAPI = (id, updatedItem) => axios.patch(`${url}/${id}`, updatedItem);
+const deleteItemAPI = (id) => axios.delete(`${url}/${id}`)
+const fetchItemsBySearchAPI = (searchQuery) => axios.get(`${url}/search?searchQuery=${searchQuery.search || 'none'}`)
 
 export const getItems = () => async (dispatch) => {
   try {
@@ -19,6 +22,34 @@ export const createItem = (item) => async (dispatch) => {
   try {
     const { data } = await createItemAPI(item);
     dispatch({ type: "CREATE", payload: data });
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const updateItem = (id, post) => async (dispatch) => {
+  try {
+    const { data } = await updateItemAPI(id, post);
+    dispatch({ type: "UPDATE", payload: data });
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const deleteItem = (id) => async (dispatch) => {
+  try {
+    await deleteItemAPI(id);
+    dispatch({ type: "DELETE", payload: id })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const getItemsBySearch = (searchQuery) => async (dispatch) => {
+  try {
+    const { data: { data } } = await fetchItemsBySearchAPI(searchQuery);
+    dispatch({ type: "FETCH_BY_SEARCH", payload: data });
+    console.log('data:', data)
   } catch (err) {
     console.log(err)
   }
