971b013efcf4bf4c13c57700fc64b4efff5b1ecb
AlisherU <alisherrahmonov@gmail.com>
2021-09-04 10:41:15 +0900

    fix: remove rented items from main page

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from 'react-router-dom'
 import { postOrder } from "../actions/orderActions";
+import { updateItem } from "../actions/itemActions";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -15,9 +16,9 @@ import { ethers } from 'ethers';
 import dotenv from "dotenv";
 dotenv.config();
 
- 
+
 let url;
-process.env.REACT_APP_ENVIRONMENT === "PROD" ? (url ='http://13.212.157.177/stripe/create-checkout-session') : (url = 'http://localhost:4000/stripe/create-checkout-session')
+process.env.REACT_APP_ENVIRONMENT === "PROD" ? (url = 'http://13.212.157.177/stripe/create-checkout-session') : (url = 'http://localhost:4000/stripe/create-checkout-session')
 
 
 const useStyles = makeStyles((theme) => ({
@@ -56,7 +57,7 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   buttonClear: {
-    marginBottom: 10, 
+    marginBottom: 10,
   },
   downArrowButton: {
     border: 'none',
@@ -99,7 +100,12 @@ const Order = () => {
   const { userInfo } = userLogin
 
   const itemOrder = useSelector(state => state.createOrder)
-  const order  = itemOrder.tempOrder
+  const order = itemOrder.tempOrder
+
+  const itemList = useSelector(state => state.itemList)
+  const { items } = itemList
+
+
 
   const [orderData, setOrderData] = useState({
     title: "",
@@ -118,16 +124,16 @@ const Order = () => {
   })
 
   const ETHJPY = 412633   // spot rate for ETHPY, can make dynamic in the future
- 
+
   const sendEmail = (e) => {
     e.preventDefault();
     emailjs.sendForm(process.env.REACT_APP_EMAIL_CLIENT, process.env.REACT_APP_EMAIL_TEMPLATE, e.target, process.env.REACT_APP_EMAIL_USER)
       .then((result) => {
-          console.log(result.text);
+        console.log(result.text);
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
-    }
+  }
 
   const calcNights = (start, end) => {
     return parseInt((new Date(end) - new Date(start)) / (1000 * 60 * 60 * 24), 10)
@@ -135,21 +141,21 @@ const Order = () => {
 
   const sendToStripe = async () => {
     const res = await fetch(`${url}/${order.price_id}/${calcNights(orderData.startDate, orderData.returnDate)}`, {
-          method: 'POST',
-          headers: {
-            "Content-Type": 'application/json',
-          }
-        })
-        const body = await res.json()
-        window.location.href = body.url
-    }
+      method: 'POST',
+      headers: {
+        "Content-Type": 'application/json',
+      }
+    })
+    const body = await res.json()
+    window.location.href = body.url
+  }
 
   const handleEthPayment = async () => {
     setError()
     await startEthPayment({
-      setError, 
+      setError,
       setTxs,
-      ether: `${(totalPrice/ETHJPY).toFixed(5)}`,
+      ether: `${(totalPrice / ETHJPY).toFixed(5)}`,
       addr: "0x98DfcD53E4d52B6A6dBF85054A7A95B07De2C88a", // Renty's wallet
     })
   }
@@ -167,33 +173,37 @@ const Order = () => {
         value: ethers.utils.parseEther(ether)
       })
       setTxs(tx.hash)
-      dispatch(postOrder({ ...orderData, txhash: tx.hash, ethprice: ether}))
+      dispatch(postOrder({ ...orderData, txhash: tx.hash, ethprice: ether }))
       setTimeout(redirect, 1000)
     } catch (err) {
       setError(err.message)
     }
   }
-  
+
   const redirect = () => {
     return history.push('/profile')
   }
 
   const handlePaymentSubmit = (e) => {
     e.preventDefault();
+    const itemToUpdate = items.filter(item => item.price_id === orderData.price_id)
+    dispatch(updateItem((itemToUpdate[0]._id), ({ rented: true })))
 
-    if(payment === "credit") {
+
+    if (payment === "credit") {
       // sendEmail(e) // turn off emails here
       dispatch(postOrder(orderData))
-      setTimeout(sendToStripe, 2000) 
+      setTimeout(sendToStripe, 12000)
 
     } else {
       // sendEmail(e) // turn off emails here
       handleEthPayment()
-    } 
+    }
   }
 
   const handlePaymentMethod = (e) => {
     setPayment(e.target.value)
+
   }
 
   const totalPrice = orderData.nightPrice * calcNights(orderData.startDate, orderData.returnDate)
@@ -202,126 +212,126 @@ const Order = () => {
   return (
     <div>
 
-    { order ? 
-    <Paper className={classes.paper}>
-        { order ? <Typography className={classes.label}>{order.title}</Typography> : null }
-        { order ? <Typography className={classes.label}>{order.description}</Typography> : null }
-      <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handlePaymentSubmit}>
-        <br></br>
-        <FormControl className={classes.formControl}>
-        <InputLabel>Payment Method</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={payment}
-            onChange={handlePaymentMethod}
-          >
-            <MenuItem value={"credit"}>Credit Card</MenuItem>
-            <MenuItem value={"crypto"}>Ethereum</MenuItem>
-          </Select>
-        </FormControl>
+      {order ?
+        <Paper className={classes.paper}>
+          {order ? <Typography className={classes.label}>{order.title}</Typography> : null}
+          {order ? <Typography className={classes.label}>{order.description}</Typography> : null}
+          <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handlePaymentSubmit}>
+            <br></br>
+            <FormControl className={classes.formControl}>
+              <InputLabel>Payment Method</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={payment}
+                onChange={handlePaymentMethod}
+              >
+                <MenuItem value={"credit"}>Credit Card</MenuItem>
+                <MenuItem value={"crypto"}>Ethereum</MenuItem>
+              </Select>
+            </FormControl>
 
-        <Typography className={classes.label}>Input Start date: </Typography>
-        <TextField
-          id="startdate"
-          name="startDate"
-          type="date"
-          variant="outlined"
-          fullWidth
-          onChange={(e) => setOrderData({ 
-            ...orderData, 
-            startDate: e.target.value, 
-            title: order.title, 
-            price_id: order.price_id, 
-            nightPrice: parseInt(order.price),
-            lenderEmail: order.email,
-            lenderName: order.name,
-            renterEmail: userInfo.email,
-            renterName: userInfo.name,
-            selectedFile: order.selectedFile,
-          })}
-        />
-        <br></br>
-        <Typography className={classes.label}>Input Return date: </Typography>
+            <Typography className={classes.label}>Input Start date: </Typography>
+            <TextField
+              id="startdate"
+              name="startDate"
+              type="date"
+              variant="outlined"
+              fullWidth
+              onChange={(e) => setOrderData({
+                ...orderData,
+                startDate: e.target.value,
+                title: order.title,
+                price_id: order.price_id,
+                nightPrice: parseInt(order.price),
+                lenderEmail: order.email,
+                lenderName: order.name,
+                renterEmail: userInfo.email,
+                renterName: userInfo.name,
+                selectedFile: order.selectedFile,
+              })}
+            />
+            <br></br>
+            <Typography className={classes.label}>Input Return date: </Typography>
 
-        <TextField
-          id="returndate"
-          type="date"
-          name="returnDate"
-          variant="outlined"
-          fullWidth
-          onChange={(e) => setOrderData({ ...orderData, returnDate: e.target.value, })}
-        />
-        <Typography className={classes.label}>Total rental nights: {calcNights(orderData.startDate, orderData.returnDate) ? calcNights(orderData.startDate, orderData.returnDate) : 0}</Typography>
-        <TextField
-          className={classes.hideTextField}
-          id="numberNights"
-          name="numberNights"
-          variant="outlined"
-          label="Number of nights calculated"
-          value={totalPrice ? totalPrice : 0}
-          fullWidth
-        />
-        <TextField
-          className={classes.hideTextField}
-          id="name"
-          type="text"
-          name="name"
-          variant="outlined"
-          label="name"
-          value={order.name}
-          fullWidth
-        />
-        <TextField
-          className={classes.hideTextField}
-          id="email"
-          type="text"
-          name="email"
-          variant="outlined"
-          label="email"
-          value={order.email}
-          fullWidth
-        />
-        <TextField
-          className={classes.hideTextField}
-          id="message"
-          type="text"
-          name="message"
-          variant="outlined"
-          label="message"
-          value={order.description}
-          fullWidth
-        />
-        <TextField
-          className={classes.hideTextField}
-          id="subject"
-          type="text"
-          name="subject"
-          variant="outlined"
-          label="subject"
-          value="Congrats on your rental."
-          fullWidth
-        />
-        <TextField
-          className={classes.hideTextField}
-          id="recipient"
-          type="text"
-          name="recipient"
-          variant="outlined"
-          label="recipient"
-          value={userInfo.email}
-          fullWidth
-        /> 
-        <Button className={classes.buttonSubmit} variant="contained" size="large" type="submit">
-          { payment === "credit" ? `Pay with credit: ${`¥` + Intl.NumberFormat().format(totalPrice)}` : `Pay now in ETH: ${(totalPrice/ETHJPY).toFixed(5)}`}
-        </Button>
-      </form>
-      {/* <Button className={classes.buttonSubmit} size="large" variant="contained" onClick={handleEthPayment}>Pay in ETH</Button> */}
-      <p>{error}</p>
-        <p>{ txs && `Your transaction hash: ${txs}`}</p>
-    </Paper>
+            <TextField
+              id="returndate"
+              type="date"
+              name="returnDate"
+              variant="outlined"
+              fullWidth
+              onChange={(e) => setOrderData({ ...orderData, returnDate: e.target.value, })}
+            />
+            <Typography className={classes.label}>Total rental nights: {calcNights(orderData.startDate, orderData.returnDate) ? calcNights(orderData.startDate, orderData.returnDate) : 0}</Typography>
+            <TextField
+              className={classes.hideTextField}
+              id="numberNights"
+              name="numberNights"
+              variant="outlined"
+              label="Number of nights calculated"
+              value={totalPrice ? totalPrice : 0}
+              fullWidth
+            />
+            <TextField
+              className={classes.hideTextField}
+              id="name"
+              type="text"
+              name="name"
+              variant="outlined"
+              label="name"
+              value={order.name}
+              fullWidth
+            />
+            <TextField
+              className={classes.hideTextField}
+              id="email"
+              type="text"
+              name="email"
+              variant="outlined"
+              label="email"
+              value={order.email}
+              fullWidth
+            />
+            <TextField
+              className={classes.hideTextField}
+              id="message"
+              type="text"
+              name="message"
+              variant="outlined"
+              label="message"
+              value={order.description}
+              fullWidth
+            />
+            <TextField
+              className={classes.hideTextField}
+              id="subject"
+              type="text"
+              name="subject"
+              variant="outlined"
+              label="subject"
+              value="Congrats on your rental."
+              fullWidth
+            />
+            <TextField
+              className={classes.hideTextField}
+              id="recipient"
+              type="text"
+              name="recipient"
+              variant="outlined"
+              label="recipient"
+              value={userInfo.email}
+              fullWidth
+            />
+            <Button className={classes.buttonSubmit} variant="contained" size="large" type="submit">
+              {payment === "credit" ? `Pay with credit: ${`¥` + Intl.NumberFormat().format(totalPrice)}` : `Pay now in ETH: ${(totalPrice / ETHJPY).toFixed(5)}`}
+            </Button>
+          </form>
+          {/* <Button className={classes.buttonSubmit} size="large" variant="contained" onClick={handleEthPayment}>Pay in ETH</Button> */}
+          <p>{error}</p>
+          <p>{txs && `Your transaction hash: ${txs}`}</p>
+        </Paper>
 
-   : <button><Link to="/main">Go back</Link></button>  }
+        : <button><Link to="/main">Go back</Link></button>}
 
     </div>
   )
